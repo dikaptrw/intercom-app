c6be8cbda632e1d3eadc759c3ecc38ab3094e47e
dikaptrw <dikaptrw@gmail.com>
2025-08-21 10:27:50 +0800

    feat(meeting-room): provide video type toogle button

--- src/pages/meeting-room/index.tsx
+++ src/pages/meeting-room/index.tsx
@@ -5,9 +5,10 @@ import {
   useMeetingManager,
   useMeetingStatus,
   useVideoInputs,
+  VideoTileGrid,
 } from 'amazon-chime-sdk-component-library-react';
 import ControlBar from './control-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MeetingSessionConfiguration } from 'amazon-chime-sdk-js';
 import { AWS_CHIME_API } from '@/utils/constants/api';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -23,6 +24,9 @@ function MeetingRoomPage() {
   const meetingManager = useMeetingManager();
   const audioVideo = useAudioVideo();
   const { devices, selectedDevice } = useVideoInputs();
+  const [videoType, setVideoType] = useState<'custom' | 'video-tile-grid'>(
+    'custom',
+  );
 
   console.log({ devices, selectedDevice, audioVideo });
 
@@ -90,11 +94,26 @@ function MeetingRoomPage() {
     <div className="bg-[#1b1c20]">
       <GlobalStyles />
 
-      {/* Renders all active meeting tiles, including your local video */}
-      <VideoGrid />
-      {/* <div className="h-[calc(100vh-80px)] p-4">
-        <VideoTileGrid />
-      </div> */}
+      <button
+        className="cursor-pointer absolute top-8 right-8 z-[50] bg-gray-800 px-3 py-2 text-sm rounded flex items-center"
+        onClick={() =>
+          setVideoType(videoType === 'custom' ? 'video-tile-grid' : 'custom')
+        }
+      >
+        <span className="text-white">
+          {videoType === 'custom'
+            ? 'Switch to Video Tile Grid'
+            : 'Switch to Custom View'}
+        </span>
+      </button>
+
+      {videoType === 'custom' && <VideoGrid />}
+
+      {videoType === 'video-tile-grid' && (
+        <div className="h-[calc(100vh-80px)] !p-4 !border-b !border-white/5">
+          <VideoTileGrid />
+        </div>
+      )}
 
       <ControlBar />
     </div>
