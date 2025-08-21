425ee4986a4b682615ad36e87c7cadc5775e2ea9
dikaptrw <dikaptrw@gmail.com>
2025-08-21 18:18:56 +0800

    style(room): update grid and participant styles

--- src/pages/room/participant-tile.tsx
+++ src/pages/room/participant-tile.tsx
@@ -1,5 +1,5 @@
 import type { Participant } from '@/utils/constants/meeting';
-import { getInitials } from '@/utils/functions';
+import { cn, getInitials } from '@/utils/functions';
 import {
   LocalVideo,
   RemoteVideo,
@@ -9,43 +9,56 @@ import {
 
 export interface ParticipantTileProps {
   participant?: Participant;
+  className?: string;
 }
 
 const FallbackInitials = ({ name }: { name: string }) => {
   const initials = getInitials(name);
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-[5] bg- text-white p-1 rounded-full w-[15%] aspect-square flex items-center justify-center font-medium uppercase [container-type:inline-size] bg-room-primary">
-      <span className="!text-[40cqw]">{initials}</span>
+    <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-[5] bg- text-white p-1 rounded-full h-[20%] md:h-[30%] aspect-square flex items-center justify-center font-medium uppercase [container-type:inline-size] bg-room-primary">
+      <span className="!text-[35cqw] md:!text-[40cqw]">{initials}</span>
     </div>
   );
 };
 
-const ParticipantTile = ({ participant }: ParticipantTileProps) => {
+const ParticipantTile = ({ participant, className }: ParticipantTileProps) => {
   const { isVideoEnabled } = useLocalVideo();
 
   return (
-    <div className="relative w-full h-full bg-room-foreground rounded-lg overflow-hidden flex items-center justify-center">
-      <div className="aspect-video w-full max-h-full">
-        {participant?.isLocal ? (
-          isVideoEnabled ? (
-            <LocalVideo className="w-full h-full object-cover" />
+    <div
+      className={cn(
+        'relative w-full h-full bg-room-foreground rounded-lg overflow-hidden flex items-center justify-center',
+        className,
+      )}
+    >
+      <div className="flex items-center justify-center w-full h-full">
+        {/* Aspect ratio video inside */}
+        <div className="w-full h-full max-w-full max-h-full">
+          {participant?.isLocal ? (
+            isVideoEnabled ? (
+              <LocalVideo className="!bg-room-foreground [&>.ch-video]:!aspect-video [&>.ch-video]:!object-contain w-full h-full" />
+            ) : (
+              <FallbackInitials name={participant?.name || 'You'} />
+            )
+          ) : participant?.tileId ? (
+            <RemoteVideo
+              tileId={participant?.tileId}
+              className="!bg-room-foreground [&>.ch-video]:!aspect-video [&>.ch-video]:!object-contain w-full h-full"
+            />
           ) : (
-            <FallbackInitials name={participant?.name || 'You'} />
-          )
-        ) : participant?.tileId ? (
-          <RemoteVideo
-            tileId={participant?.tileId}
-            className="w-full h-full object-cover"
-          />
-        ) : (
-          <FallbackInitials name={participant?.name || 'Anonymous'} />
-        )}
+            <FallbackInitials name={participant?.name || 'Anonymous'} />
+          )}
+        </div>
       </div>
 
       <div className="[&_.ch-subtitle]:!text-sm absolute bottom-2 left-3 right-2 bg-opacity-50 text-white p-1 font-medium">
         <RosterAttendee
           attendeeId={participant?.attendeeId || ''}
-          subtitle={participant?.name}
+          subtitle={
+            participant?.isLocal
+              ? `${participant?.name} (You)`
+              : participant?.name || 'Anonymous'
+          }
         />
       </div>
     </div>
--- src/pages/room/video-grid.tsx
+++ src/pages/room/video-grid.tsx
@@ -5,20 +5,45 @@ import { useParticipants } from '@/hooks/useParticipants';
 const VideoGrid = () => {
   const participants = useParticipants();
 
-  console.log('Participants PAGE:', participants);
+  console.log('Participants:', participants);
 
   return (
-    <div
-      className={cn(
-        'grid gap-4 items-center justify-center w-full h-[calc(100vh-80px)] !p-4 !border-b !border-white/5',
-        participants.length > 0 ? 'grid-cols-2' : 'grid-cols-1',
-      )}
-    >
-      {participants?.map((participant) => {
-        return (
-          <ParticipantTile key={participant.tileId} participant={participant} />
-        );
-      })}
+    <div className="h-[calc(100vh-80px)] w-full flex items-center justify-center">
+      <div
+        className={cn(
+          'max-w-[1240px] h-[calc(100vh-80px)] grid gap-4 items-center justify-center w-full !p-4 !border-b !border-white/5',
+          participants.length <= 1
+            ? 'grid-cols-1'
+            : participants.length === 2
+              ? 'grid-cols-2'
+              : participants.length <= 4
+                ? 'grid-cols-2'
+                : participants.length <= 6
+                  ? 'grid-cols-3'
+                  : 'grid-cols-4',
+        )}
+      >
+        {participants?.map((participant, i) => {
+          const isLastRowStart =
+            participants.length % 2 !== 0 && i === participants.length - 1;
+
+          return (
+            <div
+              key={participant.tileId}
+              className={cn(
+                'flex h-full w-full justify-center items-center',
+                // if last item in an odd row, center it
+                isLastRowStart ? 'col-span-full flex justify-center' : '',
+              )}
+            >
+              <ParticipantTile
+                className={cn(isLastRowStart ? 'max-w-[calc(50%-8px)]' : '')}
+                participant={participant}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
