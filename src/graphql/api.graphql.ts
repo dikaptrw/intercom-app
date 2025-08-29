d89c1dddf0a298d77dc99a0e7763ddcb97d27260
dikaptrw <dikaptrw@gmail.com>
2025-08-29 21:50:14 +0800

    chore: update graphql operation and generated file

--- src/graphql/api.graphql.ts
+++ src/graphql/api.graphql.ts
@@ -1,6 +1,11 @@
 // @ts-ignore
 
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  useQuery,
+  useMutation,
+  UseQueryOptions,
+  UseMutationOptions,
+} from '@tanstack/react-query';
 import { useFetchData } from '../hooks/useGraphqlFetcher';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
@@ -68,6 +73,21 @@ export type UnitsQuery = {
   units?: Array<{ __typename?: 'Unit'; id: string; name: string }> | null;
 };
 
+export type SessionQueryVariables = Exact<{
+  id: Scalars['ID']['input'];
+}>;
+
+export type SessionQuery = { __typename?: 'Query'; session: boolean };
+
+export type CallMutationVariables = Exact<{
+  id: Scalars['ID']['input'];
+}>;
+
+export type CallMutation = {
+  __typename?: 'Mutation';
+  call: { __typename?: 'CallSession'; id: string };
+};
+
 export const UnitsDocument = /*#__PURE__*/ `
     query units {
   units {
@@ -102,6 +122,80 @@ useUnitsQuery.fetcher = (
 ) =>
   useFetchData<UnitsQuery, UnitsQueryVariables>(
     UnitsDocument,
+    variables,
+    options,
+  );
+
+export const SessionDocument = /*#__PURE__*/ `
+    query session($id: ID!) {
+  session(id: $id)
+}
+    `;
+
+export const useSessionQuery = <TData = SessionQuery, TError = unknown>(
+  variables: SessionQueryVariables,
+  options?: Omit<UseQueryOptions<SessionQuery, TError, TData>, 'queryKey'> & {
+    queryKey?: UseQueryOptions<SessionQuery, TError, TData>['queryKey'];
+  },
+) => {
+  return useQuery<SessionQuery, TError, TData>({
+    queryKey: ['session', variables],
+    queryFn: useFetchData<SessionQuery, SessionQueryVariables>(
+      SessionDocument,
+      variables,
+    ),
+    ...options,
+  });
+};
+
+useSessionQuery.getKey = (variables: SessionQueryVariables) => [
+  'session',
+  variables,
+];
+
+useSessionQuery.fetcher = (
+  variables: SessionQueryVariables,
+  options?: RequestInit['headers'],
+) =>
+  useFetchData<SessionQuery, SessionQueryVariables>(
+    SessionDocument,
+    variables,
+    options,
+  );
+
+export const CallDocument = /*#__PURE__*/ `
+    mutation call($id: ID!) {
+  call(id: $id) {
+    id
+  }
+}
+    `;
+
+export const useCallMutation = <TError = unknown, TContext = unknown>(
+  options?: UseMutationOptions<
+    CallMutation,
+    TError,
+    CallMutationVariables,
+    TContext
+  >,
+) => {
+  return useMutation<CallMutation, TError, CallMutationVariables, TContext>({
+    mutationKey: ['call'],
+    mutationFn: (variables?: CallMutationVariables) =>
+      useFetchData<CallMutation, CallMutationVariables>(
+        CallDocument,
+        variables,
+      )(),
+    ...options,
+  });
+};
+
+useCallMutation.fetcher = (
+  variables: CallMutationVariables,
+  options?: RequestInit['headers'],
+) =>
+  useFetchData<CallMutation, CallMutationVariables>(
+    CallDocument,
     variables,
     options,
   );
